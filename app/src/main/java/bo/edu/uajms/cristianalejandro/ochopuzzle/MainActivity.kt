b713335925577de6ce2530a3f3b6ee76804ef640
lobopolenta22-rgb <lobopolenta22@gmail.com>
2026-09-04 00:23:14 -0300

    Primer commit del proyecto OchoPuzzle

---
+++ app/src/main/java/bo/edu/uajms/cristianalejandro/ochopuzzle/MainActivity.kt
@@ -0,0 +1,16 @@
+package bo.edu.uajms.cristianalejandro.ochopuzzle
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
